66b5c3b06051017d97849a54d2ff3447c3c71043
endpointclients <endpointclients@gmail.com>
2026-02-28 01:19:55 +0500

    TEST PUSH

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-[url('/images/jlc.jpeg')] bg-center bg-cover bg-no-repeat w-full h-dvh"></div>
+    <div className="bg-[url('/images/jlc.jpeg')] bg-center bg-cover bg-no-repeat w-full h-dvh">
+      JLC
+    </div>
   );
 }
